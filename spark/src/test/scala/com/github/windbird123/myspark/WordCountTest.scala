790e1e4873fe6554717ae8851db2f410c5540656
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-05-20 20:26:30 +0900

    test

--- spark/src/test/scala/com/github/windbird123/myspark/WordCountTest.scala
+++ spark/src/test/scala/com/github/windbird123/myspark/WordCountTest.scala
@@ -3,12 +3,14 @@ package com.github.windbird123.myspark
 /**
  * A simple test for everyone's favourite wordcount example.
  */
-
 import com.holdenkarau.spark.testing.SharedSparkContext
 import org.scalatest.FunSuite
 
 class WordCountTest extends FunSuite with SharedSparkContext {
-  test("word count with Stop Words Removed"){
-   assert(true)
+  test("word count with Stop Words Removed") {
+    val linesRDD =
+      sc.parallelize(Seq("How happy was the panda? You ask.", "Panda is the most happy panda in all the#!?ing land!"))
+
+    assert(true)
   }
 }
